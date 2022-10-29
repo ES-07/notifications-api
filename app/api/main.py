86d3fdc34da70afe7cec6eef65dac0766c582de2
Ricardo Rodriguez <ricardorodriguez@ua.pt>
2022-10-29 23:31:31 +0100

    Intrusion API - initiation

--- app/api/main.py
+++ app/api/main.py
@@ -19,6 +19,7 @@ def alive():
 
 @app.post("/intrusion/")
 async def create_item(intrusion: Intrusion):
+    
     print("Intrusion received:")
     print(intrusion)
     intrusion_dict = intrusion.dict()
@@ -28,25 +29,20 @@ async def create_item(intrusion: Intrusion):
     return intrusion
 
 async def request_email(client, camera_id):
+
     print(f"Requesting email for the camera {camera_id}")
     response = await client.get(f"sites-api/{camera_id}")
     return response.text
 
 def send_email(intrusion_dict):
-    # Open a plain text file for reading.  For this example, assume that
-    # the text file contains only ASCII characters.
-    
+
     information = f"NEW INTRUSION DETECTED AT {intrusion_dict.timestamp} | FRAME: {intrusion_dict.frame_id} | CAMERA: {intrusion_dict.camera_id}\nThis is an automatic message!"
     msg = MIMEText(information, 'plain', 'utf-8')
-
-    # me == the sender's email address
-    # you == the recipient's email address
     msg['Subject'] = Header("Intrusion detected!", 'utf-8')
     msg['From'] = "cctv-es007@gmail.com"
     msg['To'] = email
 
-    # Send the message via our own SMTP server, but don't include the
-    # envelope header.
+    # Send the message via our own SMTP server (localhost)
     s = smtplib.SMTP('localhost')
     s.sendmail("cctv-es007@gmail.com", [email], msg.as_string())
     s.quit()
